069e0f072d6e0938215788db812f095ccf68af92
Guillermo Martinez <dumblemoe@gmail.com>
2019-07-26 21:29:08 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,7 +3,7 @@
 describe "./calculator.rb" do
  first_number = 1
  second_number = 2
- sum = first_number+second_number
+
 
   it "contains a local variable called first_number that is assigned to a number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
