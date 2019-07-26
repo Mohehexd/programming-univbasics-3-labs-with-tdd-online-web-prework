7f89242b7d2bcb43a6c859ce92eeebf1d8cde9d2
Guillermo Martinez <dumblemoe@gmail.com>
2019-07-26 21:14:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -9,6 +9,4 @@
   second_number = get_variable_from_file('./calculator.rb, "second_number')
   sum = get_variable_from_file('./calculator.rb, first_number+second_number')
   expect(first_number).to be_an(Integer).or be_a(Float)
-  expect(second_number).not_to equal(0)
-  expect (sum = first_number+second_number)
 end
