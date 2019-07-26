795851e382924e7f1a769695e7d2b80a51421c4f
Guillermo Martinez <dumblemoe@gmail.com>
2019-07-26 21:11:03 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
  first_number = 1
  second_number = 2
- sum = first_number + second_number
+ sum = first_number+second_number
  
  "contains a local variable called first_number, second_number that is assigned to a number" do
  "contains a local variable with the sum of first_number and second_number"
@@ -10,4 +10,5 @@
   sum = get_variable_from_file('./calculator.rb, first_number+second_number')
   expect(first_number).to be_an(Integer).or be_a(Float)
   expect(second_number).not_to equal(0)
+  expect (sum = first_number+second_number)
 end
