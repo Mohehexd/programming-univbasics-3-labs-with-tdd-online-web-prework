857cccad2492a62720d769600ff25a1be8f9a93f
Guillermo Martinez <dumblemoe@gmail.com>
2019-07-26 21:02:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,10 @@
 # Add your variables here
  first_number = 1
+ second_number = 2
  
- "contains a local variable called first_number that is assigned to a number" do
+ "contains a local variable called first_number, second_number that is assigned to a number" do
   first_number = get_variable_from_file('./calculator.rb', "first_number")
- 
+  second_number = get_variable_from_file('./calculator.rb, "second_number')
   expect(first_number).to be_an(Integer).or be_a(Float)
+  expect(second_number).not_to equal(0)
 end
